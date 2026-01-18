192f5bd63dcb11c7f138e5db2e5a4f672483dfb6
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-01-18 19:03:36 +0530

    File updated at 1/18/2026, 7:03:36 PM

--- 401-BinaryWatch/401-BinaryWatch.cpp
+++ 401-BinaryWatch/401-BinaryWatch.cpp
@@ -1,20 +1,32 @@
-// Last updated: 1/18/2026, 6:52:42 PM
+// Last updated: 1/18/2026, 7:03:36 PM
 1class Solution {
 2public:
-3    vector<string> readBinaryWatch(int turnedOn) {
-4        vector<string>res;
-5        for(int hour=0;hour<12;hour++){
-6            for(int minute=0;minute<60;minute++){
-7                int setBits=__builtin_popcount(hour) + __builtin_popcount(minute);
-8                if(setBits==turnedOn){
-9                    string time=to_string(hour)+":";
-10                    if(minute<10)
-11                        time+="0";
-12                    time+=to_string(minute);
-13                    res.push_back(time);
-14                }
-15            }
-16        }
-17        return res;
-18    }
-19};
+3    vector<string> readBinaryWatch(int n) {
+4        vector<string> ans;
+5        for (int i = 0; i < 12; i++) {
+6            int onBits = setBits(i);
+7            if (onBits > n)
+8                continue;
+9            for (int j = 0; j < 60; j++) {
+10                int onBits_j = setBits(j);
+11                if (onBits + onBits_j == n) {
+12                    string temp = to_string(i) + ":";
+13                    if (j < 10) {
+14                        temp = temp + "0";
+15                    }
+16                    temp = temp + to_string(j);
+17                    ans.push_back(temp);
+18                }
+19            }
+20        }
+21        return ans;
+22    }
+23    int setBits(int n) {
+24        int count = 0;
+25        while (n > 0) {
+26            n = n & (n - 1);
+27            count++;
+28        }
+29        return count;
+30    }
+31};
