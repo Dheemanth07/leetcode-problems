8eedbe2d01aa9742ca207d265816428c2cda5bbe
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-02-08 09:27:49 +0530

    Create 981-TimeBasedKey-ValueStore

---
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.cpp
@@ -0,0 +1,28 @@
+// Last updated: 2/8/2026, 9:27:49 AM
+1class TimeMap {
+2public:
+3    unordered_map<string, vector<pair<int, string>>> mp;
+4    TimeMap() {}
+5
+6    void set(string key, string value, int timestamp) {
+7        mp[key].push_back({timestamp, value});
+8    }
+9
+10    string get(string key, int timestamp) {
+11        if (mp.find(key) == mp.end())
+12            return "";
+13        string res = "";
+14        auto& p = mp[key];
+15        int l = 0, r = p.size() - 1;
+16        while (l <= r) {
+17            int mid = l + (r - l) / 2;
+18            if (p[mid].first <= timestamp) {
+19                res = p[mid].second;
+20                l = mid + 1;
+21            } else
+22                r = mid - 1;
+23        }
+24        return res;
+25    }
+26};
+27
