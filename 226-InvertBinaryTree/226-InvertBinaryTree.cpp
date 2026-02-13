8fc59d6053bce6b7360937805941bd740ba458f1
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-02-13 20:52:07 +0530

    File updated at 2/13/2026, 8:52:20 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.cpp
+++ 226-InvertBinaryTree/226-InvertBinaryTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/15/2026, 12:18:04 PM
+// Last updated: 2/13/2026, 8:52:20 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -7,16 +7,18 @@
 6 *     TreeNode *right;
 7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    TreeNode* invertTree(TreeNode* root) {
-15        if(!root) return 0;
-16        TreeNode* node=root->left;
-17        root->left=invertTree(root->right);
-18        root->right=invertTree(node);
-19        return root;
-20    }
-21};
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+10 * right(right) {}
+11 * };
+12 */
+13class Solution {
+14public:
+15    TreeNode* invertTree(TreeNode* root) {
+16        if (!root)
+17            return 0;
+18        TreeNode* node = new TreeNode(root->val);
+19        node->left = invertTree(root->right);
+20        node->right = invertTree(root->left);
+21        return node;
+22    }
+23};
