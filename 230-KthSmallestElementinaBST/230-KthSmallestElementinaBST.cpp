5485428625427a9ee9f963f1178116c40a8e779c
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-02-18 20:51:00 +0530

    File updated at 2/18/2026, 8:51:02 PM

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.cpp
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/16/2026, 10:27:10 PM
+// Last updated: 2/18/2026, 8:51:02 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -7,28 +7,34 @@
 6 *     TreeNode *right;
 7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12
-13class Solution {
-14public:
-15    int kthSmallest(TreeNode* root, int k) {
-16        stack<TreeNode*>st;
-17        TreeNode*cur=root;
-18        while(cur||!st.empty()){
-19            while(cur){
-20                st.push(cur);
-21                cur=cur->left;
-22            }
-23        cur=st.top();
-24        st.pop();
-25        k--;
-26        if(k==0)
-27            return cur->val;
-28        cur=cur->right;
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+10 * right(right) {}
+11 * };
+12 */
+13
+14class Solution {
+15public:
+16    int count = 0;
+17    int ans = 0;
+18
+19    void inorder(TreeNode* root, int k) {
+20        if (!root)
+21            return;
+22
+23        inorder(root->left, k);
+24
+25        count++;
+26        if (count == k) {
+27            ans = root->val;
+28            return;
 29        }
-30    return -1;
-31    }
-32};
+30
+31        inorder(root->right, k);
+32    }
 33
+34    int kthSmallest(TreeNode* root, int k) {
+35        inorder(root, k);
+36        return ans;
+37    }
+38};
+39
