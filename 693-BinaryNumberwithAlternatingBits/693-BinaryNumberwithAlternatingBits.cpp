9e99387f1b1105084268865438bdb48947c1334b
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-02-18 16:48:25 +0530

    File updated at 2/18/2026, 4:48:37 PM

--- 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.cpp
+++ 693-BinaryNumberwithAlternatingBits/693-BinaryNumberwithAlternatingBits.cpp
@@ -1,16 +1,13 @@
-// Last updated: 2/18/2026, 4:47:42 PM
+// Last updated: 2/18/2026, 4:48:37 PM
 1class Solution {
 2public:
 3    bool hasAlternatingBits(int n) {
-4        int prevBit = n % 2;
-5        n >>= 1;
-6        while (n) {
-7            int currBit = n % 2;
-8            if (currBit == prevBit)
-9                return false;
-10            prevBit = currBit;
-11            n >>= 1;
-12        }
-13        return true;
-14    }
-15};
+4        // Step 1: XOR n with itself shifted right by 1
+5        // Using 'long' to prevent overflow when doing x + 1
+6        long x = n ^ (n >> 1);
+7
+8        // Step 2: Check if x is a number consisting of all 1s
+9        // Trick: (x & (x + 1)) == 0 only works for numbers like 3(11), 7(111), 15(1111)
+10        return (x & (x + 1)) == 0;
+11    }
+12};
