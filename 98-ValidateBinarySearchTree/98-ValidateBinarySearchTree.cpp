174ab16de96d0466e608bde79c30fbd34c2e5667
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-02-18 20:25:17 +0530

    File updated at 2/18/2026, 8:25:19 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/15/2026, 10:59:16 PM
+// Last updated: 2/18/2026, 8:25:19 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -7,18 +7,22 @@
 6 *     TreeNode *right;
 7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
 8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    bool validate(TreeNode* root,long long minval,long long maxval){
-15        if(!root) return true;
-16        if(root->val<=minval || root->val>=maxval) return false;
-17        return validate(root->left,minval,root->val) && validate(root->right,root->val,maxval);
-18    }
-19
-20    bool isValidBST(TreeNode* root) {
-21        return validate(root,LLONG_MIN,LLONG_MAX);
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+10 * right(right) {}
+11 * };
+12 */
+13class Solution {
+14public:
+15    bool validate(TreeNode* root, long long minval, long long maxval) {
+16        if (!root)
+17            return true;
+18        if (root->val <= minval || root->val >= maxval)
+19            return false;
+20        return validate(root->left, minval, root->val) &&
+21               validate(root->right, root->val, maxval);
 22    }
-23};
+23
+24    bool isValidBST(TreeNode* root) {
+25        return validate(root, LLONG_MIN, LLONG_MAX);
+26    }
+27};
