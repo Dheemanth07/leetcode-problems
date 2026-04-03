9fb3fc0934122fd539a8cb5feab70f585937924a
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-04-03 21:49:02 +0530

    File updated at 4/3/2026, 9:49:05 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.cpp
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -1,22 +1,24 @@
-// Last updated: 1/25/2026, 6:38:36 PM
+// Last updated: 4/3/2026, 9:49:05 PM
 1class Solution {
 2public:
 3    vector<int> topKFrequent(vector<int>& nums, int k) {
-4        if(nums.empty()) return {};
-5        int n=nums.size();
-6        unordered_map<int,int>mp;
-7        for(int num:nums)
-8            mp[num]++;
-9        vector<vector<int>>bucket(n+1);
-10        for(auto &p:mp)
-11            bucket[p.second].push_back(p.first);
-12        vector<int>res;
-13        for(int i=bucket.size()-1;i>=0 && res.size()<k ;i--){
-14            for(int num:bucket[i]){
-15                res.push_back(num);
-16                if(res.size()==k) break;
-17            }
-18        }
-19        return res;
-20    }
-21};
+4        if (nums.empty())
+5            return {};
+6        int n = nums.size();
+7        unordered_map<int, int> mp;
+8        for (int num : nums)
+9            mp[num]++;
+10        vector<vector<int>> bucket(n + 1);
+11        for (auto& p : mp)
+12            bucket[p.second].push_back(p.first);
+13        vector<int> res;
+14        for (int i = bucket.size() - 1; i >= 0 && res.size() < k; i--) {
+15            for (int num : bucket[i]) {
+16                res.push_back(num);
+17                if (res.size() == k)
+18                    break;
+19            }
+20        }
+21        return res;
+22    }
+23};
