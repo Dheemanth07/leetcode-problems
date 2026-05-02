2654a76bb14003087660e513b9891cef2a557cb6
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-02 19:16:34 +0530

    File updated at 5/2/2026, 7:16:42 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.cpp
+++ 875-KokoEatingBananas/875-KokoEatingBananas.cpp
@@ -1,20 +1,41 @@
-// Last updated: 2/7/2026, 8:18:37 PM
+// Last updated: 5/2/2026, 7:16:42 PM
 1class Solution {
 2public:
 3    int minEatingSpeed(vector<int>& piles, int h) {
-4        int left = 1;
-5        int right = *max_element(piles.begin(), piles.end());
-6        while (left < right) {
-7            int mid = left + (right - left) / 2;
-8            long total = 0;
-9            for (int p : piles)
-10                total += (p + mid - 1) / mid;
-11            if (total <= h)
-12                right = mid;
-13            else
-14                left = mid + 1;
-15        }
-16        return left;
-17    }
-18};
-19
+4        // Pillar 1: The Search Space
+5        int left = 1;
+6
+7        // Find the maximum pile to set our upper bound
+8        int right = 0;
+9        for (int p : piles) {
+10            right = std::max(right, p);
+11        }
+12
+13        int best_speed = right; // Store the best valid answer we find
+14
+15        while (left <= right) {
+16            int mid = left + (right - left) / 2;
+17
+18            // Pillar 2: The Test
+19            long long total_hours = 0;
+20            for (int p : piles) {
+21                // Integer math trick for Ceiling Division: ceil(a/b) = (a + b -
+22                // 1) / b
+23                total_hours += (p + mid - 1) / mid;
+24            }
+25
+26            // Pillar 3: The Decision
+27            if (total_hours <= h) {
+28                // Fast enough! Record it, but try to find a slower valid speed
+29                best_speed = mid;
+30                right = mid - 1;
+31            } else {
+32                // Too slow! We must eat faster
+33                left = mid + 1;
+34            }
+35        }
+36
+37        return best_speed;
+38    }
+39};
+40
