ecb0e0688dd23cb7ca719b483f02feb24dfaeb7f
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-02 18:15:57 +0530

    File updated at 5/2/2026, 6:16:04 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.cpp
@@ -1,18 +1,18 @@
-// Last updated: 2/5/2026, 10:37:01 PM
+// Last updated: 5/2/2026, 6:16:04 PM
 1class Solution {
 2public:
 3    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-4        int m = matrix.size();
-5        int n = matrix[0].size();
+4        int rows = matrix.size();
+5        int cols = matrix[0].size();
 6
 7        int left = 0;
-8        int right = m * n - 1;
+8        int right = rows * cols - 1;
 9
 10        while (left <= right) {
 11            int mid = left + (right - left) / 2;
 12
 13            // Map 1D index back to 2D coordinates
-14            int midValue = matrix[mid / n][mid % n];
+14            int midValue = matrix[mid / cols][mid % cols];
 15
 16            if (midValue == target) {
 17                return true;
