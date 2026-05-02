571da2a9453486e1f54a8abe9549faeb4ecbdae2
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-02 16:18:32 +0530

    File updated at 5/2/2026, 4:18:38 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.cpp
@@ -1,35 +1,36 @@
-// Last updated: 2/3/2026, 8:03:05 PM
+// Last updated: 5/2/2026, 4:18:38 PM
 1class Solution {
 2public:
 3    string minWindow(string s, string t) {
-4        if (s.empty() || t.empty())
+4        if (t.empty() || s.length() < t.length())
 5            return "";
-6        unordered_map<char, int> target, window;
-7        for (char c : t)
-8            target[c]++;
-9        int left = 0, right = 0, startIdx = 0;
-10        int required = target.size();
-11        int formed = 0;
-12        int minLen = INT_MAX;
-13        while (right < s.length()) {
-14            char c = s[right];
-15            window[c]++;
-16            if (target.count(c) && window[c] == target[c])
-17                formed++;
-18            while (left <= right && formed == required) {
-19                char c = s[left];
-20                if (right - left + 1 < minLen) {
-21                    minLen = right - left + 1;
-22                    startIdx = left;
-23                }
-24                window[c]--;
-25                if (target.count(c) && window[c] < target[c])
-26                    formed--;
-27                left++;
-28            }
-29            right++;
-30        }
-31        return minLen == INT_MAX ? "" : s.substr(startIdx, minLen);
-32    }
-33};
-34
+6
+7        unordered_map<char, int> target, window;
+8        for (char c : t)
+9            target[c]++;
+10        int left = 0, right = 0, startIdx = 0;
+11        int required = target.size();
+12        int formed = 0;
+13        int minLen = INT_MAX;
+14        while (right < s.length()) {
+15            char c = s[right];
+16            window[c]++;
+17            if (target.count(c) && window[c] == target[c])
+18                formed++;
+19            while (left <= right && formed == required) {
+20                char c = s[left];
+21                if (right - left + 1 < minLen) {
+22                    minLen = right - left + 1;
+23                    startIdx = left;
+24                }
+25                window[c]--;
+26                if (target.count(c) && window[c] < target[c])
+27                    formed--;
+28                left++;
+29            }
+30            right++;
+31        }
+32        return minLen == INT_MAX ? "" : s.substr(startIdx, minLen);
+33    }
+34};
+35
