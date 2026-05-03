6d257f8527db5c8fa2bb55fca15443f9ec81bb3b
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-03 15:44:32 +0530

    File updated at 5/3/2026, 3:44:42 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cpp
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -1,42 +1,55 @@
-// Last updated: 5/3/2026, 12:08:59 PM
-1#include <string>
-2
-3class Solution {
-4public:
-5    int strStr(string haystack, string needle) {
-6        int n = haystack.length();
-7        int m = needle.length();
-8
-9        // Edge case: An empty needle is technically always found at index 0
-10        if (m == 0)
-11            return 0;
-12
-13        // Loop 'i' is our Starting Line.
-14        // We only go up to (n - m) because anything after that is too short to
-15        // hold the needle.
-16        for (int i = 0; i <= n - m; i++) {
-17
-18            int j = 0;
-19
-20            // Loop 'j' is our Runner.
-21            // It checks if the characters match, offset by our starting line
-22            // 'i'.
-23            while (j < m && haystack[i + j] == needle[j]) {
-24                j++; // Match! Take another step.
-25            }
+// Last updated: 5/3/2026, 3:44:42 PM
+1class Solution {
+2public:
+3    vector<int> computeLPS(string pattern) {
+4        int m = pattern.size();
+5        vector<int> lps(m, 0);
+6
+7        int len = 0;
+8        int i = 1;
+9
+10        while (i < m) {
+11            if (pattern[i] == pattern[len]) {
+12                len++;
+13                lps[i] = len;
+14                i++;
+15            } else {
+16                if (len != 0) {
+17                    len = lps[len - 1];
+18                } else {
+19                    lps[i] = 0;
+20                    i++;
+21                }
+22            }
+23        }
+24        return lps;
+25    }
 26
-27            // If the Runner made it to the very end of the needle, we found it!
-28            if (j == m) {
-29                return i;
-30            }
+27    int strStr(string haystack, string needle) {
+28        int n = haystack.size(), m = needle.size();
+29        if (m == 0)
+30            return 0;
 31
-32            // If we didn't return, the while loop broke early due to a
-33            // mismatch. The outer 'for' loop will automatically increment 'i'
-34            // and we try again.
-35        }
-36
-37        // If we check every single valid starting line and never return, it
-38        // doesn't exist.
-39        return -1;
-40    }
-41};
+32        vector<int> lps = computeLPS(needle);
+33
+34        int i = 0, j = 0;
+35
+36        while (i < n) {
+37            if (haystack[i] == needle[j]) {
+38                i++;
+39                j++;
+40            }
+41
+42            if (j == m) {
+43                return i - j;
+44            } else if (i < n && haystack[i] != needle[j]) {
+45                if (j != 0)
+46                    j = lps[j - 1];
+47                else
+48                    i++;
+49            }
+50        }
+51
+52        return -1;
+53    }
+54};
