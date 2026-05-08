acf8af663315e06fd0f48063eb969e5cb4583d22
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-08 08:36:45 +0530

    File updated at 5/8/2026, 8:36:52 AM

--- 146-LRUCache/146-LRUCache.cpp
+++ 146-LRUCache/146-LRUCache.cpp
@@ -1,4 +1,4 @@
-// Last updated: 2/12/2026, 5:47:58 PM
+// Last updated: 5/8/2026, 8:36:52 AM
 1class LRUCache {
 2    int size;
 3    list<int> lru_list;
@@ -10,24 +10,30 @@
 9    int get(int key) {
 10        if (mp.find(key) == mp.end())
 11            return -1;
-12        lru_list.splice(lru_list.begin(), lru_list, mp[key].second);
-13        return mp[key].first;
-14    }
-15
-16    void put(int key, int value) {
-17        if (mp.count(key)) {
-18            // Update value and move to front
-19            mp[key].first = value;
-20            lru_list.splice(lru_list.begin(), lru_list, mp[key].second);
-21            return;
-22        }
-23        if (lru_list.size() >= size) {
-24            int last_key = lru_list.back();
-25            lru_list.pop_back();
-26            mp.erase(last_key);
-27        }
-28        lru_list.push_front(key);
-29        mp[key] = {value, lru_list.begin()};
-30    }
-31};
-32
+12
+13        lru_list.splice(lru_list.begin(), lru_list, mp[key].second);
+14
+15        return mp[key].first;
+16    }
+17
+18    void put(int key, int value) {
+19        if (mp.count(key)) {
+20            // Update value and move to front
+21            mp[key].first = value;
+22
+23            lru_list.splice(lru_list.begin(), lru_list, mp[key].second);
+24
+25            return;
+26        }
+27
+28        if (lru_list.size() >= size) {
+29            int last_key = lru_list.back();
+30            lru_list.pop_back();
+31            mp.erase(last_key);
+32        }
+33
+34        lru_list.push_front(key);
+35        mp[key] = {value, lru_list.begin()};
+36    }
+37};
+38
