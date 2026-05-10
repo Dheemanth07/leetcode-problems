6f27263a954af5ff955133b326359d6091aacbbd
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-10 16:21:32 +0530

    File updated at 5/10/2026, 4:21:36 PM

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.cpp
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.cpp
@@ -1,25 +1,27 @@
-// Last updated: 5/10/2026, 4:21:03 PM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-8 * };
-9 */
-10class Solution {
-11public:
-12    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-13        if (!root || root == p || root == q)
-14            return root;
-15
-16        TreeNode* leftReport = lowestCommonAncestor(root->left, p, q);
-17        TreeNode* rightReport = lowestCommonAncestor(root->right, p, q);
-18
-19        if (leftReport && rightReport)
-20            return root;
+// Last updated: 5/10/2026, 4:21:36 PM
+1class Solution {
+2public:
+3    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+4        // 1. The Base Cases
+5        // If we hit the bottom, return null.
+6        // If we ARE one of the targets, immediately return ourselves!
+7        if (root == nullptr || root == p || root == q) {
+8            return root;
+9        }
+10
+11        // 2. The Leap of Faith (Wait for the reports)
+12        TreeNode* leftReport = lowestCommonAncestor(root->left, p, q);
+13        TreeNode* rightReport = lowestCommonAncestor(root->right, p, q);
+14
+15        // 3. Post-Processing: Evaluate the reports
+16
+17        // THE WINNING CASE: Both departments found a target. We are the LCA!
+18        if (leftReport != nullptr && rightReport != nullptr) {
+19            return root;
+20        }
 21
-22        return leftReport ? leftReport : rightReport;
-23    }
-24};
+22        // THE PASS-IT-UP CASE: If one side is null, return the side that ISN'T
+23        // null. (If both are null, this conveniently just returns null anyway).
+24        return leftReport != nullptr ? leftReport : rightReport;
+25    }
+26};
