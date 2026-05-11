e72b4f7abe507493fe42ef232eb1c45196baf665
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-11 17:18:18 +0530

    File updated at 5/11/2026, 5:18:29 PM

--- 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.cpp
+++ 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/11/2026, 5:14:41 PM
+// Last updated: 5/11/2026, 5:18:29 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -17,43 +17,39 @@
 16            return "N,";
 17
 18        // Preorder: Root, Left, Right
-19        string result = to_string(root->val) + ",";
-20        result += serialize(root->left);
-21        result += serialize(root->right);
-22
-23        return result;
-24    }
-25
-26    // --- DESERIALIZE: String to Tree ---
-27    TreeNode* deserialize(string data) {
-28        // Load the string into a stringstream (the conveyor belt)
-29        stringstream ss(data);
-30        return buildTree(ss);
-31    }
-32
-33private:
-34    // Notice we pass the stringstream by reference (&) so it constantly moves
-35    // forward!
-36    TreeNode* buildTree(stringstream& ss) {
-37        string token;
-38        // Grab the next item off the conveyor belt, stopping at the comma
-39        getline(ss, token, ',');
+19        return to_string(root->val) + "," + serialize(root->left) + serialize(root->right);
+20    }
+21
+22    // --- DESERIALIZE: String to Tree ---
+23    TreeNode* deserialize(string data) {
+24        // Load the string into a stringstream (the conveyor belt)
+25        stringstream ss(data);
+26        return buildTree(ss);
+27    }
+28
+29private:
+30    // Notice we pass the stringstream by reference (&) so it constantly moves
+31    // forward!
+32    TreeNode* buildTree(stringstream& ss) {
+33        string token;
+34        // Grab the next item off the conveyor belt, stopping at the comma
+35        getline(ss, token, ',');
+36
+37        if (token == "N") {
+38            return nullptr; // We hit a ghost! Branch ends here.
+39        }
 40
-41        if (token == "N") {
-42            return nullptr; // We hit a ghost! Branch ends here.
-43        }
-44
-45        // 1. Build the Root
-46        TreeNode* root = new TreeNode(stoi(token));
+41        // 1. Build the Root
+42        TreeNode* root = new TreeNode(stoi(token));
+43
+44        // 2. The Leap of Faith (Preorder forces Left, then Right)
+45        root->left = buildTree(ss);
+46        root->right = buildTree(ss);
 47
-48        // 2. The Leap of Faith (Preorder forces Left, then Right)
-49        root->left = buildTree(ss);
-50        root->right = buildTree(ss);
+48        return root;
+49    }
+50};
 51
-52        return root;
-53    }
-54};
-55
-56// Your Codec object will be instantiated and called as such:
-57// Codec ser, deser;
-58// TreeNode* ans = deser.deserialize(ser.serialize(root));
+52// Your Codec object will be instantiated and called as such:
+53// Codec ser, deser;
+54// TreeNode* ans = deser.deserialize(ser.serialize(root));
