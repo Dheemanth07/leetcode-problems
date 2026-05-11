28ce598c2ca95c32114c8f3f680653512a2f884f
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-11 17:14:30 +0530

    File updated at 5/11/2026, 5:14:41 PM

--- 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.cpp
+++ 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 2/20/2026, 9:19:49 PM
+// Last updated: 5/11/2026, 5:14:41 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -8,49 +8,49 @@
 7 *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 8 * };
 9 */
-10class Codec {
-11public:
-12    // Serialize
-13    void serializeHelper(TreeNode* root, string& s) {
-14        if (!root) {
-15            s += "#,";
-16            return;
-17        }
-18        s += to_string(root->val) + ",";
-19        serializeHelper(root->left, s);
-20        serializeHelper(root->right, s);
-21    }
+10
+11class Codec {
+12public:
+13    // --- SERIALIZE: Tree to String ---
+14    string serialize(TreeNode* root) {
+15        if (root == nullptr)
+16            return "N,";
+17
+18        // Preorder: Root, Left, Right
+19        string result = to_string(root->val) + ",";
+20        result += serialize(root->left);
+21        result += serialize(root->right);
 22
-23    string serialize(TreeNode* root) {
-24        string s;
-25        serializeHelper(root, s);
-26        return s;
-27    }
-28
-29    // Deserialize
-30    TreeNode* deserializeHelper(queue<string>& q) {
-31        string val = q.front();
-32        q.pop();
-33
-34        if (val == "#")
-35            return nullptr;
-36
-37        TreeNode* node = new TreeNode(stoi(val));
-38        node->left = deserializeHelper(q);
-39        node->right = deserializeHelper(q);
-40        return node;
-41    }
-42
-43    TreeNode* deserialize(string data) {
-44        string token;
-45        stringstream ss(data);
-46        queue<string> q;
+23        return result;
+24    }
+25
+26    // --- DESERIALIZE: String to Tree ---
+27    TreeNode* deserialize(string data) {
+28        // Load the string into a stringstream (the conveyor belt)
+29        stringstream ss(data);
+30        return buildTree(ss);
+31    }
+32
+33private:
+34    // Notice we pass the stringstream by reference (&) so it constantly moves
+35    // forward!
+36    TreeNode* buildTree(stringstream& ss) {
+37        string token;
+38        // Grab the next item off the conveyor belt, stopping at the comma
+39        getline(ss, token, ',');
+40
+41        if (token == "N") {
+42            return nullptr; // We hit a ghost! Branch ends here.
+43        }
+44
+45        // 1. Build the Root
+46        TreeNode* root = new TreeNode(stoi(token));
 47
-48        while (getline(ss, token, ',')) {
-49            q.push(token);
-50        }
+48        // 2. The Leap of Faith (Preorder forces Left, then Right)
+49        root->left = buildTree(ss);
+50        root->right = buildTree(ss);
 51
-52        return deserializeHelper(q);
+52        return root;
 53    }
 54};
 55
