8d94d4dd5bbeec25f8c5c02ad4ae3cb9296cbbfc
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-12 22:45:48 +0530

    File updated at 5/12/2026, 10:46:03 PM

--- 15-3Sum/15-3Sum.cpp
+++ 15-3Sum/15-3Sum.cpp
@@ -1,30 +1,41 @@
-// Last updated: 4/4/2026, 12:33:20 PM
+// Last updated: 5/12/2026, 10:46:03 PM
 1class Solution {
 2public:
 3    vector<vector<int>> threeSum(vector<int>& nums) {
 4        vector<vector<int>> res;
-5        int n = nums.size();
-6        sort(nums.begin(), nums.end());
-7        for (int i = 0; i < n - 2; i++) {
-8            if (i > 0 && nums[i] == nums[i - 1])
-9                continue;
-10            int l = i + 1, r = n - 1;
-11            while (l < r) {
-12                int sum = nums[i] + nums[l] + nums[r];
-13                if (!sum) {
-14                    res.push_back({nums[i], nums[l], nums[r]});
-15                    while (l < r && nums[l] == nums[l + 1])
-16                        l++; // Skip duplicates
-17                    while (l < r && nums[r] == nums[r - 1])
-18                        r--; // Skip duplicates
-19                    l++;
-20                    r--;
-21                } else if (sum < 0)
-22                    l++;
-23                else
-24                    r--;
-25            }
-26        }
-27        return res;
-28    }
-29};
+5
+6        int n = nums.size();
+7
+8        sort(nums.begin(), nums.end());
+9
+10        for (int i = 0; i < n - 2; i++) {
+11
+12            if (i > 0 && nums[i] == nums[i - 1])
+13                continue;
+14
+15            int l = i + 1, r = n - 1;
+16
+17            while (l < r) {
+18                int sum = nums[i] + nums[l] + nums[r];
+19
+20                if (!sum) {
+21                    res.push_back({nums[i], nums[l], nums[r]});
+22
+23                    while (l < r && nums[l] == nums[l + 1])
+24                        l++; // Skip duplicates
+25
+26                    while (l < r && nums[r] == nums[r - 1])
+27                        r--; // Skip duplicates
+28
+29                    l++;
+30                    r--;
+31                } else if (sum < 0)
+32                    l++;
+33                else
+34                    r--;
+35            }
+36        }
+37
+38        return res;
+39    }
+40};
