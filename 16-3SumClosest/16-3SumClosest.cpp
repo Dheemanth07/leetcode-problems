ef521f2c0d23ded117b2efc0c2d6b0038835fb23
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-05-12 22:46:35 +0530

    Create 16-3SumClosest

---
+++ 16-3SumClosest/16-3SumClosest.cpp
@@ -0,0 +1,34 @@
+// Last updated: 5/12/2026, 10:46:51 PM
+1class Solution {
+2public:
+3    int threeSumClosest(vector<int>& nums, int target) {
+4        sort(nums.begin(), nums.end());
+5
+6        int n = nums.size();
+7
+8        int minDiff = INT_MAX;
+9        int minSum = 0;
+10
+11        for (int i = 0; i < n - 2; i++) {
+12            int l = i + 1, r = n - 1;
+13
+14            while (l < r) {
+15                int sum = nums[i] + nums[l] + nums[r];
+16                int diff = abs(target - sum);
+17
+18                if (diff < minDiff) {
+19                    minDiff = diff;
+20                    minSum = sum;
+21                }
+22
+23                if (sum < target)
+24                    l++;
+25                else if (sum > target)
+26                    r--;
+27                else
+28                    return sum;
+29            }
+30        }
+31        return minSum;
+32    }
+33};
