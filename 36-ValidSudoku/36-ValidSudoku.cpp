c06dfa71a3f7c4446c787a294a229fa7f855fbbc
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-15 11:15:13 +0530

    File updated at 6/15/2026, 11:15:15 AM

--- 36-ValidSudoku/36-ValidSudoku.cpp
+++ 36-ValidSudoku/36-ValidSudoku.cpp
@@ -1,4 +1,4 @@
-// Last updated: 4/3/2026, 9:50:23 PM
+// Last updated: 6/15/2026, 11:15:15 AM
 1class Solution {
 2public:
 3    bool isValidSudoku(vector<vector<char>>& board) {
@@ -8,19 +8,20 @@
 7        for (int i = 0; i < 9; ++i) {
 8            for (int j = 0; j < 9; ++j) {
 9                char c = board[i][j];
-10                if (c == '.') continue;
-11
-12                int num = c - '1';  // Map '1'-'9' to 0-8
-13                int boxIndex = (i / 3) * 3 + (j / 3);
-14
-15                // Check row, column, and 3x3 box
-16                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num])
-17                    return false;
-18
-19                // Mark as seen
-20                rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
-21            }
-22        }
-23        return true;
-24    }
-25};
+10                
+11                if (c == '.') continue;
+12
+13                int num = c - '1';  // Map '1'-'9' to 0-8
+14                int boxIndex = (i / 3) * 3 + (j / 3);
+15
+16                // Check row, column, and 3x3 box
+17                if (rows[i][num] || cols[j][num] || boxes[boxIndex][num])
+18                    return false;
+19
+20                // Mark as seen
+21                rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+22            }
+23        }
+24        return true;
+25    }
+26};
