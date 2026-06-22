d59b77dd6dd86e8167bc224ff8a8c1406d1b1a3f
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-22 18:29:57 +0530

    Create 1544-CountGoodNodesInBinaryTree

---
+++ 1544-CountGoodNodesInBinaryTree/1544-CountGoodNodesInBinaryTree.cpp
@@ -0,0 +1,34 @@
+// Last updated: 6/22/2026, 6:30:01 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int count = 0;
+
+    void dfs(TreeNode* root, int maxSoFar) {
+        if (!root)
+            return;
+        if (root->val >= maxSoFar) {
+            count++;
+            maxSoFar = root->val;
+        }
+        dfs(root->left, maxSoFar);
+        dfs(root->right, maxSoFar);
+    }
+
+    int goodNodes(TreeNode* root) {
+        dfs(root, root->val);
+        return count;
+    }
+};
