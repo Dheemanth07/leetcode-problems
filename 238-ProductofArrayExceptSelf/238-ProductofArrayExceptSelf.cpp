68204f1b7f841597ca0688d4e3e38de03ceab592
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-22 19:39:46 +0530

    File updated at 6/22/2026, 7:39:51 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.cpp
@@ -1,19 +1,24 @@
-// Last updated: 4/3/2026, 9:49:55 PM
+// Last updated: 6/22/2026, 7:39:51 PM
 1class Solution {
 2public:
 3    vector<int> productExceptSelf(vector<int>& nums) {
 4        int n = nums.size();
 5        vector<int> answer(n, 1);
-6        int leftproduct = 1;
-7        for (int i = 0; i < n; i++) {
-8            answer[i] = leftproduct;
-9            leftproduct *= nums[i];
-10        }
-11        int rightproduct = 1;
-12        for (int i = n - 1; i >= 0; i--) {
-13            answer[i] *= rightproduct;
-14            rightproduct *= nums[i];
-15        }
-16        return answer;
-17    }
-18};
+6
+7        int leftproduct = 1;
+8
+9        for (int i = 0; i < n; i++) {
+10            answer[i] = leftproduct;
+11            leftproduct *= nums[i];
+12        }
+13
+14        int rightproduct = 1;
+15
+16        for (int i = n - 1; i >= 0; i--) {
+17            answer[i] *= rightproduct;
+18            rightproduct *= nums[i];
+19        }
+20
+21        return answer;
+22    }
+23};
