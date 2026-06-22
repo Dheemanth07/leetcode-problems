2c5d6c564d29cfda34ccc068d669aed77218eb95
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-22 19:45:26 +0530

    File updated at 6/22/2026, 7:45:31 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,19 +1,19 @@
-// Last updated: 5/2/2026, 9:45:40 AM
+// Last updated: 6/22/2026, 7:45:31 PM
 1class Solution {
 2public:
 3    int subarraySum(vector<int>& nums, int k) {
 4        int current_sum = 0;
 5        int count = 0;
-6        unordered_map<int, int> prefix_map;
-7        prefix_map[0] = 1;
-8
-9        for (int num : nums) {
-10            current_sum += num;
-11
-12            int target = current_sum - k;
-13            if (prefix_map.find(target) != prefix_map.end()) {
-14                count += prefix_map[target];
-15            }
+6
+7        unordered_map<int, int> prefix_map;
+8        prefix_map[0] = 1;
+9
+10        for (int num : nums) {
+11            current_sum += num;
+12
+13            int target = current_sum - k;
+14            if (prefix_map.find(target) != prefix_map.end()) 
+15                count += prefix_map[target];
 16
 17            prefix_map[current_sum]++;
 18        }
