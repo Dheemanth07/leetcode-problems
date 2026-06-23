05b5a1086c6b37851a878d0fb85b75197724012b
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-23 17:38:49 +0530

    File updated at 6/23/2026, 5:38:53 PM

--- 567-PermutationinString/567-PermutationinString.cpp
+++ 567-PermutationinString/567-PermutationinString.cpp
@@ -1,27 +1,33 @@
-// Last updated: 5/2/2026, 4:26:38 PM
+// Last updated: 6/23/2026, 5:38:53 PM
 1class Solution {
 2public:
 3    bool checkInclusion(string s1, string s2) {
 4        int n1 = s1.length(), n2 = s2.length();
-5        vector<int> freq1(26, 0), freq2(26, 0);
-6        if (n1 > n2)
-7            return false;
-8        for (int i = 0; i < n1; i++) {
-9            freq1[s1[i] - 'a']++;
-10            freq2[s2[i] - 'a']++;
-11        }
-12        if (freq1 == freq2)
-13            return true;
-14
-15        int left = 0;
-16        for (int right = n1; right < n2; right++) {
-17            freq2[s2[right] - 'a']++;
-18            freq2[s2[left] - 'a']--;
-19            left++;
+5
+6        vector<int> freq1(26, 0), freq2(26, 0);
+7
+8        if (n1 > n2)
+9            return false;
+10
+11        for (int i = 0; i < n1; i++) {
+12            freq1[s1[i] - 'a']++;
+13            freq2[s2[i] - 'a']++;
+14        }
+15
+16        if (freq1 == freq2)
+17            return true;
+18
+19        int left = 0;
 20
-21            if (freq1 == freq2)
-22                return true;
-23        }
-24        return false;
-25    }
-26};
+21        for (int right = n1; right < n2; right++) {
+22            freq2[s2[right] - 'a']++;
+23            freq2[s2[left] - 'a']--;
+24            left++;
+25
+26            if (freq1 == freq2)
+27                return true;
+28        }
+29
+30        return false;
+31    }
+32};
