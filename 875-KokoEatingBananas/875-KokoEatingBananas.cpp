385819f2642329fff946a178305e812499f8baa3
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-24 12:50:24 +0530

    File updated at 6/24/2026, 12:50:29 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.cpp
+++ 875-KokoEatingBananas/875-KokoEatingBananas.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/2/2026, 7:16:42 PM
+// Last updated: 6/24/2026, 12:50:29 PM
 1class Solution {
 2public:
 3    int minEatingSpeed(vector<int>& piles, int h) {
@@ -6,36 +6,31 @@
 5        int left = 1;
 6
 7        // Find the maximum pile to set our upper bound
-8        int right = 0;
-9        for (int p : piles) {
-10            right = std::max(right, p);
-11        }
-12
-13        int best_speed = right; // Store the best valid answer we find
+8        int right = *max_element(piles.begin(), piles.end());
+9
+10        int best_speed = right; // Store the best valid answer we find
+11
+12        while (left <= right) {
+13            int mid = left + (right - left) / 2;
 14
-15        while (left <= right) {
-16            int mid = left + (right - left) / 2;
-17
-18            // Pillar 2: The Test
-19            long long total_hours = 0;
-20            for (int p : piles) {
-21                // Integer math trick for Ceiling Division: ceil(a/b) = (a + b -
-22                // 1) / b
-23                total_hours += (p + mid - 1) / mid;
-24            }
-25
-26            // Pillar 3: The Decision
-27            if (total_hours <= h) {
-28                // Fast enough! Record it, but try to find a slower valid speed
-29                best_speed = mid;
-30                right = mid - 1;
-31            } else {
-32                // Too slow! We must eat faster
-33                left = mid + 1;
-34            }
-35        }
-36
-37        return best_speed;
-38    }
-39};
-40
+15            // Pillar 2: The Test
+16            long long total_hours = 0;
+17            for (int p : piles)
+18                // Integer math trick for Ceiling Division: ceil(a/b) = (a + b -
+19                // 1) / b
+20                total_hours += (p + mid - 1) / mid;
+21
+22            // Pillar 3: The Decision
+23            if (total_hours <= h) {
+24                // Fast enough! Record it, but try to find a slower valid speed
+25                best_speed = mid;
+26                right = mid - 1;
+27            } else
+28                // Too slow! We must eat faster
+29                left = mid + 1;
+30        }
+31
+32        return best_speed;
+33    }
+34};
+35
