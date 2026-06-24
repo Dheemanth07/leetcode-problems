bb23f58404d7951c10ed9be516778fd7aa4718a2
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-24 18:19:30 +0530

    File updated at 6/24/2026, 6:19:38 PM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,17 +1,18 @@
-// Last updated: 6/24/2026, 6:18:09 PM
+// Last updated: 6/24/2026, 6:19:38 PM
 1class Solution {
 2public:
 3    int mySqrt(int x) {
-4        if (x == 0)
+4        if (!x)
 5            return 0;
 6
-7        long t = x; // Start with x as our initial guess
-8
-9        // While our guess squared is greater than x, keep refining
-10        while (t * t > x) {
-11            t = (t + x / t) / 2;
-12        }
-13
-14        return t;
-15    }
-16};
+7        // Apply the formula: e^(0.5 * ln(x))
+8        int ans = exp(0.5 * log(x));
+9
+10        // The safety check for floating point precision
+11        // If (ans + 1) squared is still <= x, we know the math undershot.
+12        if ((long long)(ans + 1) * (ans + 1) <= x) 
+13            return ans + 1;
+14
+15        return ans;
+16    }
+17};
