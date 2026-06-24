3e95cfe93a14ca8847dfac160b90552e5854dc7c
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-24 14:39:25 +0530

    File updated at 6/24/2026, 2:39:31 PM

--- 162-FindPeakElement/162-FindPeakElement.cpp
+++ 162-FindPeakElement/162-FindPeakElement.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/2/2026, 7:27:06 PM
+// Last updated: 6/24/2026, 2:39:31 PM
 1class Solution {
 2public:
 3    int findPeakElement(std::vector<int>& nums) {
@@ -9,17 +9,16 @@
 8            int mid = left + (right - left) / 2;
 9
 10            // Look at the slope exactly one step to the right
-11            if (nums[mid] < nums[mid + 1]) {
+11            if (nums[mid] < nums[mid + 1]) 
 12                // The slope is going UP. A peak MUST exist to the right.
 13                left = mid + 1;
-14            } else {
+14            else 
 15                // The slope is going DOWN. A peak MUST exist to the left
 16                // (or we are currently standing on the peak).
 17                right = mid;
-18            }
-19        }
-20
-21        // When left == right, we have narrowed our search to exactly one peak
-22        return left;
-23    }
-24};
+18        }
+19
+20        // When left == right, we have narrowed our search to exactly one peak
+21        return left;
+22    }
+23};
