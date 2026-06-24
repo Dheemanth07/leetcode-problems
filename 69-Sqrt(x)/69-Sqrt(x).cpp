c944182563e43b67e6b795604772b3ffe022467a
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-24 18:18:01 +0530

    File updated at 6/24/2026, 6:18:09 PM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,18 +1,17 @@
-// Last updated: 6/24/2026, 6:16:09 PM
+// Last updated: 6/24/2026, 6:18:09 PM
 1class Solution {
 2public:
 3    int mySqrt(int x) {
-4        int l = 1;
-5        int r = x;
-6        while (l <= r) {
-7            int m = (r - l) / 2 + l;
-8            if (m > x / m)
-9                r = m - 1;
-10            else if (m < x / m)
-11                l = m + 1;
-12            else
-13                return m;
-14        }
-15        return r;
-16    }
-17};
+4        if (x == 0)
+5            return 0;
+6
+7        long t = x; // Start with x as our initial guess
+8
+9        // While our guess squared is greater than x, keep refining
+10        while (t * t > x) {
+11            t = (t + x / t) / 2;
+12        }
+13
+14        return t;
+15    }
+16};
