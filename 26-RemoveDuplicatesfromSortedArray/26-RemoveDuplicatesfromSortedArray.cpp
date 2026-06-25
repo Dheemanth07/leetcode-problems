f3b4a7b58e05dde4dd2c1b16e223b6fae42574c2
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-25 11:10:25 +0530

    File updated at 6/25/2026, 11:10:25 AM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.cpp
@@ -1,16 +1,16 @@
-// Last updated: 1/28/2026, 8:23:04 PM
+// Last updated: 6/25/2026, 11:10:25 AM
 1class Solution {
 2public:
 3    int removeDuplicates(vector<int>& nums) {
-4        if (nums.empty()) return 0;
-5        
-6        int slow = 0;
-7        for (int fast = 1; fast < nums.size(); fast++) {
-8            if (nums[fast] != nums[slow]) {
-9                slow++;
-10                nums[slow] = nums[fast];
-11            }
-12        }
+4        if (nums.empty())
+5            return 0;
+6
+7        int slow = 0;
+8        for (int fast = 1; fast < nums.size(); fast++) {
+9            if (nums[fast] != nums[slow]) 
+10                nums[++slow] = nums[fast];
+11        }
+12        
 13        return slow + 1;
 14    }
 15};
