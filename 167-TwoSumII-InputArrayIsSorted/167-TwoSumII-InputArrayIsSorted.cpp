198780b50cf594b1d19ca5660e347945357db71f
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-25 10:43:55 +0530

    File updated at 6/25/2026, 10:43:55 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.cpp
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.cpp
@@ -1,17 +1,20 @@
-// Last updated: 4/4/2026, 12:32:48 PM
+// Last updated: 6/25/2026, 10:43:55 AM
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& numbers, int target) {
 4        int n = numbers.size();
-5        int left = 0, right = n - 1;
-6        while (left < right) {
-7            if ((numbers[left] + numbers[right]) == target)
-8                return {left + 1, right + 1};
-9            else if ((numbers[left] + numbers[right]) > target)
-10                right--;
-11            else
-12                left++;
-13        }
-14        return {};
-15    }
-16};
+5
+6        int left = 0, right = n - 1;
+7
+8        while (left < right) {
+9            if ((numbers[left] + numbers[right]) == target)
+10                return {left + 1, right + 1};
+11            else if ((numbers[left] + numbers[right]) > target)
+12                right--;
+13            else
+14                left++;
+15        }
+16
+17        return {};
+18    }
+19};
