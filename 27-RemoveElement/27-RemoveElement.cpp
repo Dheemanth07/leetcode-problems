6f3c91ccd79e3f31fa46f50f18eeeb9a2fa173b5
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-25 11:07:38 +0530

    File updated at 6/25/2026, 11:07:38 AM

--- 27-RemoveElement/27-RemoveElement.cpp
+++ 27-RemoveElement/27-RemoveElement.cpp
@@ -1,13 +1,14 @@
-// Last updated: 1/28/2026, 8:09:53 PM
+// Last updated: 6/25/2026, 11:07:38 AM
 1class Solution {
 2public:
 3    int removeElement(vector<int>& nums, int val) {
-4        if(nums.empty()) return 0;
-5
-6        int j=0;
-7        for(int i=0;i<nums.size();i++)
-8            if(nums[i]!=val)
-9                nums[j++]=nums[i];
-10        return j;
-11    }
-12};
+4        if (nums.empty())
+5            return 0;
+6
+7        int j = 0;
+8        for (int i = 0; i < nums.size(); i++)
+9            if (nums[i] != val)
+10                nums[j++] = nums[i];
+11        return j;
+12    }
+13};
