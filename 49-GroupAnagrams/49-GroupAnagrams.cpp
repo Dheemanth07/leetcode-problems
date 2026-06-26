4840beb5adb07f2a9fafb3dd1335307dd437b06e
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-26 10:18:41 +0530

    File updated at 6/26/2026, 10:18:56 AM

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,16 +1,26 @@
-// Last updated: 4/3/2026, 9:48:34 PM
+// Last updated: 6/26/2026, 10:18:56 AM
 1class Solution {
 2public:
 3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-4        unordered_map<string,vector<string>>mp;
-5        for(const string &str:strs){
-6            string sortedword=str;
-7            sort(sortedword.begin(),sortedword.end());
-8            mp[sortedword].push_back(str);
-9        }
-10        vector<vector<string>>result;
-11        for(auto &i:mp)
-12            result.push_back(move(i.second));
-13        return result;
-14    }
-15};
+4        unordered_map<string, vector<string>> mp;
+5
+6        for (string str : strs) {
+7            vector<int> freq(26, 0);
+8            for (char c : str)
+9                freq[c - 'a']++;
+10
+11            string key = "";
+12            for (int i = 0; i < 26; i++)
+13                key += "#" + to_string(freq[i]);
+14
+15            mp[key].push_back(str);
+16        }
+17
+18        vector<vector<string>> result;
+19
+20        for (auto& i : mp)
+21            result.push_back(move(i.second));
+22
+23        return result;
+24    }
+25};
