77bf040b495c9072e830d3bd61d33b5f7c069688
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-26 13:45:16 +0530

    File updated at 6/26/2026, 1:45:33 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cpp
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/3/2026, 3:44:42 PM
+// Last updated: 6/26/2026, 1:45:33 PM
 1class Solution {
 2public:
 3    vector<int> computeLPS(string pattern) {
@@ -9,47 +9,43 @@
 8        int i = 1;
 9
 10        while (i < m) {
-11            if (pattern[i] == pattern[len]) {
-12                len++;
-13                lps[i] = len;
-14                i++;
-15            } else {
-16                if (len != 0) {
-17                    len = lps[len - 1];
-18                } else {
-19                    lps[i] = 0;
-20                    i++;
-21                }
-22            }
-23        }
-24        return lps;
-25    }
-26
-27    int strStr(string haystack, string needle) {
-28        int n = haystack.size(), m = needle.size();
-29        if (m == 0)
-30            return 0;
+11            if (pattern[i] == pattern[len])
+12                lps[i++] = ++len;
+13            else {
+14                if (len != 0)
+15                    len = lps[len - 1];
+16                else
+17                    lps[i++] = 0;
+18            }
+19        }
+20        return lps;
+21    }
+22
+23    int strStr(string haystack, string needle) {
+24        int n = haystack.size(), m = needle.size();
+25        if (m == 0)
+26            return 0;
+27
+28        vector<int> lps = computeLPS(needle);
+29
+30        int i = 0, j = 0;
 31
-32        vector<int> lps = computeLPS(needle);
-33
-34        int i = 0, j = 0;
-35
-36        while (i < n) {
-37            if (haystack[i] == needle[j]) {
-38                i++;
-39                j++;
-40            }
-41
-42            if (j == m) {
-43                return i - j;
-44            } else if (i < n && haystack[i] != needle[j]) {
-45                if (j != 0)
-46                    j = lps[j - 1];
-47                else
-48                    i++;
-49            }
-50        }
-51
-52        return -1;
-53    }
-54};
+32        while (i < n) {
+33            if (haystack[i] == needle[j]) {
+34                i++;
+35                j++;
+36            }
+37
+38            if (j == m)
+39                return i - j;
+40            else if (i < n && haystack[i] != needle[j]) {
+41                if (j != 0)
+42                    j = lps[j - 1];
+43                else
+44                    i++;
+45            }
+46        }
+47
+48        return -1;
+49    }
+50};
