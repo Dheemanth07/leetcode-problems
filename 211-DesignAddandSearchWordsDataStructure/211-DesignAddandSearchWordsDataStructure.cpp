1cc5e44bcbbe6491a49e9303fc6313b815b9f4ef
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-26 13:02:55 +0530

    File updated at 6/26/2026, 1:03:12 PM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.cpp
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/3/2026, 3:13:19 PM
+// Last updated: 6/26/2026, 1:03:12 PM
 1struct TrieNode {
 2    TrieNode* children[26];
 3    bool is_end_of_word;
@@ -17,7 +17,7 @@
 16
 17    // We need a helper function to keep track of what index of the word we are
 18    // on
-19    bool searchHelper(std::string& word, int index, TrieNode* node) {
+19    bool searchHelper(string& word, int index, TrieNode* node) {
 20
 21        for (int i = index; i < word.length(); i++) {
 22            char c = word[i];
@@ -28,7 +28,7 @@
 27            if (c == '.') {
 28                // We must check every possible existing child path
 29                for (int j = 0; j < 26; j++) {
-30                    if (node->children[j] != nullptr) {
+30                    if (node->children[j]) {
 31                        // Recursively search the rest of the word starting from
 32                        // this child. Notice we pass 'i + 1' to move to the
 33                        // next letter in the word!
@@ -46,7 +46,7 @@
 45            // ==========================================
 46            else {
 47                int char_idx = c - 'a';
-48                if (node->children[char_idx] == nullptr) {
+48                if (!node->children[char_idx]) {
 49                    return false; // Path doesn't exist
 50                }
 51                // Step down to the next node
@@ -61,19 +61,19 @@
 60public:
 61    WordDictionary() { root = new TrieNode(); }
 62
-63    void addWord(std::string word) {
+63    void addWord(string word) {
 64        TrieNode* current = root;
 65        for (char c : word) {
 66            int index = c - 'a';
-67            if (current->children[index] == nullptr) {
+67            if (!current->children[index])
 68                current->children[index] = new TrieNode();
-69            }
+69
 70            current = current->children[index];
 71        }
 72        current->is_end_of_word = true;
 73    }
 74
-75    bool search(std::string word) {
+75    bool search(string word) {
 76        // Start the recursive search from the root, at index 0 of the word
 77        return searchHelper(word, 0, root);
 78    }
