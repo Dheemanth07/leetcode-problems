33687e81371bf860c40b89f53b0eb0959ccd9587
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-27 11:36:56 +0530

    File updated at 6/27/2026, 11:36:56 AM

--- 739-DailyTemperatures/739-DailyTemperatures.cpp
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -1,18 +1,22 @@
-// Last updated: 5/7/2026, 8:25:52 AM
+// Last updated: 6/27/2026, 11:36:56 AM
 1class Solution {
 2public:
 3    vector<int> dailyTemperatures(vector<int>& temperatures) {
 4        int n = temperatures.size();
-5        vector<int> ans(n, 0);
-6        stack<int> st;
-7        for (int i = 0; i < n; i++) {
-8            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
-9                int prev = st.top();
-10                st.pop();
-11                ans[prev] = i - prev;
-12            }
-13            st.push(i);
-14        }
-15        return ans;
-16    }
-17};
+5
+6        vector<int> ans(n, 0);
+7        stack<int> st;
+8
+9        for (int i = 0; i < n; i++) {
+10            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+11                int prev = st.top();
+12                st.pop();
+13                ans[prev] = i - prev;
+14            }
+15
+16            st.push(i);
+17        }
+18
+19        return ans;
+20    }
+21};
