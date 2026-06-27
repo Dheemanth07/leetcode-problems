5ead28d6cc1afc71db1ccbee47e57ee2a7adbc8f
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-27 10:50:48 +0530

    File updated at 6/27/2026, 10:50:48 AM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
@@ -1,22 +1,28 @@
-// Last updated: 5/7/2026, 7:31:59 AM
-class Solution {
-public:
-    int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-
-        for (const string& t : tokens) {
-            if (t == "+" || t == "-" || t == "*" || t == "/") {
-                int b = st.top(); st.pop();
-                int a = st.top(); st.pop();
-
-                if (t == "+") st.push(a + b);
-                else if (t == "-") st.push(a - b);
-                else if (t == "*") st.push(a * b);
-                else st.push(a / b); // truncates toward 0
-            } else {
-                st.push(stoi(t));
-            }
-        }
-        return st.top();
-    }
-};
+// Last updated: 6/27/2026, 10:50:48 AM
+1class Solution {
+2public:
+3    int evalRPN(vector<string>& tokens) {
+4        stack<long> st;
+5        for (string& s : tokens) {
+6            if (s == "+" || s == "-" || s == "*" || s == "/") {
+7                long first = st.top();
+8                st.pop();
+9                long second = st.top();
+10                st.pop();
+11
+12                if (s == "+")
+13                    st.push(second + first);
+14                else if (s == "-")
+15                    st.push(second - first);
+16                else if (s == "*")
+17                    st.push(second * first);
+18                else if (s == "/")
+19                    st.push(second / first);
+20            } else
+21                st.push(stol(s));
+22        }
+23
+24        return (int)st.top();
+25    }
+26};
+27
