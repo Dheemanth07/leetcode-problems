ea9b49781c916c419ad683b12e6e6d73702ed1e2
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-06-30 18:43:15 +0530

    File updated at 6/30/2026, 6:43:20 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,4 +1,4 @@
-// Last updated: 2/10/2026, 7:07:05 PM
+// Last updated: 6/30/2026, 6:43:20 PM
 1/**
 2 * Definition for singly-linked list.
 3 * struct ListNode {
@@ -9,24 +9,30 @@
 8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
 9 * };
 10 */
-11class Solution {
-12public:
-13    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-14        ListNode dummy(0);
-15        ListNode* list3=&dummy;
-16        while(list1 && list2){
-17            if(list1->val < list2->val){
-18                list3->next=list1;
-19                list1=list1->next;
-20            }
-21            else{
-22                list3->next=list2;
-23                list2=list2->next;
-24            }
-25            list3=list3->next;
-26        }
-27        list3->next = list1 ? list1 : list2;
-28        return dummy.next;
-29
-30    }
-31};
+11
+12class Solution {
+13public:
+14    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+15
+16        ListNode dummy(0);
+17        ListNode* tail = &dummy;
+18
+19        while (list1 && list2) {
+20
+21            if (list1->val < list2->val) {
+22                tail->next = list1;
+23                list1 = list1->next;
+24            } else {
+25                tail->next = list2;
+26                list2 = list2->next;
+27            }
+28
+29            tail = tail->next;
+30        }
+31
+32        tail->next = list1 ? list1 : list2;
+33
+34        return dummy.next;
+35    }
+36};
+37
