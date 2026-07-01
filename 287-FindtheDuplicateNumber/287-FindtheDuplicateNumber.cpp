40f14ac90be23944c6625fc294d068eaa0f8a1f8
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-01 10:28:06 +0530

    File updated at 7/1/2026, 10:28:07 AM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
@@ -1,18 +1,22 @@
-// Last updated: 2/11/2026, 7:41:18 PM
+// Last updated: 7/1/2026, 10:28:07 AM
 1class Solution {
 2public:
 3    int findDuplicate(vector<int>& nums) {
 4        int slow = nums[0];
 5        int fast = nums[0];
-6        do {
-7            slow = nums[slow];
-8            fast = nums[nums[fast]];
-9        } while (slow != fast);
-10        slow = nums[0];
-11        while (slow != fast) {
-12            slow = nums[slow];
-13            fast = nums[fast];
-14        }
-15        return slow;
-16    }
-17};
+6
+7        do {
+8            slow = nums[slow];
+9            fast = nums[nums[fast]];
+10        } while (slow != fast);
+11
+12        slow = nums[0];
+13
+14        while (slow != fast) {
+15            slow = nums[slow];
+16            fast = nums[fast];
+17        }
+18
+19        return slow;
+20    }
+21};
