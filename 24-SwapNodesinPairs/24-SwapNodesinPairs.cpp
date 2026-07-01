c26da07c3788eafdf0475e744b380ba445ccd5ad
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-01 12:58:51 +0530

    File updated at 7/1/2026, 12:58:53 PM

--- 24-SwapNodesinPairs/24-SwapNodesinPairs.cpp
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.cpp
@@ -1,32 +1,29 @@
-// Last updated: 7/1/2026, 12:55:38 PM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    ListNode* swapPairs(ListNode* head) {
-14        if (!head || !head->next)
-15            return head;
-16
-17        ListNode* cur = head;
-18        ListNode* prev = nullptr;
-19        for (int i = 0; i < 2; i++) {
-20            ListNode* temp = cur->next;
-21            cur->next = prev;
-22            prev = cur;
-23            cur = temp;
-24        }
-25
-26        if (cur)
-27            head->next = swapPairs(cur);
-28
-29        return prev;
-30    }
-31};
+// Last updated: 7/1/2026, 12:58:53 PM
+1class Solution {
+2public:
+3    ListNode* swapPairs(ListNode* head) {
+4        // Create a dummy node to act as our initial 'prev'
+5        ListNode dummy(0);
+6        dummy.next = head;
+7        ListNode* prev = &dummy;
+8
+9        // We can only swap if there are at least two nodes left ahead of us
+10        while (prev->next && prev->next->next) {
+11
+12            // Identify the two nodes to swap
+13            ListNode* first = prev->next;
+14            ListNode* second = prev->next->next;
+15
+16            // The 3-Step Rewiring
+17            prev->next = second;        // Step 1
+18            first->next = second->next; // Step 2
+19            second->next = first;       // Step 3
+20
+21            // Move prev forward by 2 steps to prepare for the next pair
+22            prev = first;
+23        }
+24
+25        // Return the new head of the list
+26        return dummy.next;
+27    }
+28};
