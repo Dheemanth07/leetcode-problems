326b4d25f818e7f8ef3b5a1327ec63d1d38e88f9
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-02 18:30:22 +0530

    File updated at 7/2/2026, 6:30:27 PM

--- 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -1,37 +1,86 @@
-// Last updated: 5/8/2026, 11:39:33 AM
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Last updated: 7/2/2026, 6:30:27 PM
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool checkPallindrome(vector<int> arr){
+//         int n=arr.size();
+//         int s=0;
+//         int e=n-1;
+        
+//         while(s<=e){
+//             if(arr[s]!=arr[e]){
+//                 return 0;
+//             }
+//             s++;e--;
+//         }
+//         return 1;
+//     }
+
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> arr;
+//         ListNode* temp=head;
+//         while(temp!=NULL){
+//             arr.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         return checkPallindrome(arr);
+//     }
+// };
+
+
+
+
 class Solution {
 public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+
+        while (curr != NULL) {
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return true;
+
+        // Find the middle
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast != nullptr && fast->next != nullptr){
+
+        while (fast->next != NULL && fast->next->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
         }
-        ListNode* prev = nullptr;
-        ListNode* curr = slow;
-        while(curr != nullptr){
-            ListNode* nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-        ListNode* left = head;
-        ListNode* right = prev;
-        while(right != nullptr){
-            if(left->val != right->val) return false;
-            left = left->next;
-            right = right->next;
+
+        // Reverse second half
+        slow->next = reverse(slow->next);
+
+        // Compare both halves
+        ListNode* first = head;
+        ListNode* second = slow->next;
+
+        while (second != NULL) {
+            if (first->val != second->val)
+                return false;
+
+            first = first->next;
+            second = second->next;
         }
         return true;
     }
