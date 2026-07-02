3f205acdb78c1c50bce045963716fe9f65807fa0
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-02 18:33:10 +0530

    File updated at 7/2/2026, 6:33:15 PM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.cpp
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.cpp
@@ -1,9 +1,9 @@
-// Last updated: 5/8/2026, 12:06:23 PM
+// Last updated: 7/2/2026, 6:33:15 PM
 1class Solution {
 2public:
 3    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
 4        // If either list is empty, an intersection is impossible
-5        if (headA == nullptr || headB == nullptr)
+5        if (!headA || !headB)
 6            return nullptr;
 7
 8        ListNode* ptrA = headA;
@@ -12,10 +12,10 @@
 11        // Loop until the two pointers physically collide
 12        while (ptrA != ptrB) {
 13            // If ptrA hits the end, teleport to headB. Otherwise, step forward.
-14            ptrA = (ptrA == nullptr) ? headB : ptrA->next;
+14            ptrA = (!ptrA) ? headB : ptrA->next;
 15
 16            // If ptrB hits the end, teleport to headA. Otherwise, step forward.
-17            ptrB = (ptrB == nullptr) ? headA : ptrB->next;
+17            ptrB = (!ptrB) ? headA : ptrB->next;
 18        }
 19
 20        // When the loop breaks, ptrA and ptrB are pointing to the exact same
