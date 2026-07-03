a868f4105e61066a0ecfd4b2324e1e123bb8075c
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-03 16:46:12 +0530

    File updated at 7/3/2026, 4:46:17 PM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 6/29/2026, 7:42:22 PM
+// Last updated: 7/3/2026, 4:46:17 PM
 1/**
 2 * Definition for a binary tree node.
 3 * struct TreeNode {
@@ -11,32 +11,20 @@
 10 * right(right) {}
 11 * };
 12 */
-13class Solution {
-14public:
-15    bool isValidBST(TreeNode* root) {
-16        // Kick off the DFS with the absolute widest possible boundaries
-17        // LONG_MIN and LONG_MAX guarantee we don't overflow on INT edge cases.
-18        return validate(root, LONG_MIN, LONG_MAX);
-19    }
-20
-21private:
-22    bool validate(TreeNode* node, long long minVal, long long maxVal) {
-23        // 1. Safety Net: An empty tree is mathematically a valid BST
-24        if (node == nullptr) {
-25            return true;
-26        }
-27
-28        // 2. The Boundary Check: Did this node step out of line?
-29        // Note: BST strictly requires LESS THAN or GREATER THAN. Equal values
-30        // are invalid!
-31        if (node->val <= minVal || node->val >= maxVal) {
-32            return false;
-33        }
-34
-35        // 3. The Leap of Faith: Check both branches.
-36        // If we go Left, our current value becomes the new strict MAXIMUM.
-37        // If we go Right, our current value becomes the new strict MINIMUM.
-38        return validate(node->left, minVal, node->val) &&
-39               validate(node->right, node->val, maxVal);
-40    }
-41};
+13
+14class Solution {
+15public:
+16    bool validate(TreeNode* root, long long minval, long long maxval) {
+17        if (!root)
+18            return true;
+19        if (root->val <= minval || root->val >= maxval)
+20            return false;
+21        return validate(root->left, minval, root->val) &&
+22               validate(root->right, root->val, maxval);
+23    }
+24
+25    bool isValidBST(TreeNode* root) {
+26        return validate(root, LLONG_MIN, LLONG_MAX);
+27    }
+28};
+29
