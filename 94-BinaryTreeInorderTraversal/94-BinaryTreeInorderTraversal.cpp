f012bb0d165a20c83e0caae46d6c99b4a81d5df0
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-03 13:24:17 +0530

    File updated at 7/3/2026, 1:24:19 PM

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
@@ -1,23 +1,35 @@
-// Last updated: 2/16/2026, 3:26:40 PM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13    vector<int>res;
-14public:
-15    vector<int> inorderTraversal(TreeNode* root) {
-16        if(!root) return{};
-17        inorderTraversal(root->left);
-18        res.push_back(root->val);
-19        inorderTraversal(root->right);
-20        return res;
-21    }
-22};
+// Last updated: 7/3/2026, 1:24:19 PM
+1class Solution {
+2public:
+3    vector<int> inorderTraversal(TreeNode* root) {
+4        vector<int> result;
+5        TreeNode* cur = root;
+6
+7        while (cur) {
+8            // Rule 1: No left child
+9            if (!cur->left) {
+10                result.push_back(cur->val);
+11                cur = cur->right;
+12            } else {
+13                // Find the inorder predecessor
+14                TreeNode* predecessor = cur->left;
+15                while (predecessor->right && predecessor->right != cur)
+16                    predecessor = predecessor->right;
+17
+18                // Rule 2: Build the thread
+19                if (!predecessor->right) {
+20                    predecessor->right = cur;
+21                    cur = cur->left;
+22                }
+23                // Rule 3: Destroy the thread and collect the value
+24                else {
+25                    predecessor->right = nullptr;
+26                    result.push_back(cur->val);
+27                    cur = cur->right;
+28                }
+29            }
+30        }
+31
+32        return result;
+33    }
+34};
