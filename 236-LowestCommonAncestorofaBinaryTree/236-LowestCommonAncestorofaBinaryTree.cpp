a2c2d0467e1ca8ae443120f52372141c021e95b1
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-03 10:04:56 +0530

    File updated at 7/3/2026, 10:04:56 AM

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.cpp
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.cpp
@@ -1,27 +1,25 @@
-// Last updated: 5/10/2026, 4:21:36 PM
+// Last updated: 7/3/2026, 10:04:56 AM
 1class Solution {
 2public:
 3    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 4        // 1. The Base Cases
 5        // If we hit the bottom, return null.
 6        // If we ARE one of the targets, immediately return ourselves!
-7        if (root == nullptr || root == p || root == q) {
+7        if (!root || root == p || root == q) 
 8            return root;
-9        }
-10
-11        // 2. The Leap of Faith (Wait for the reports)
-12        TreeNode* leftReport = lowestCommonAncestor(root->left, p, q);
-13        TreeNode* rightReport = lowestCommonAncestor(root->right, p, q);
-14
-15        // 3. Post-Processing: Evaluate the reports
-16
-17        // THE WINNING CASE: Both departments found a target. We are the LCA!
-18        if (leftReport != nullptr && rightReport != nullptr) {
-19            return root;
-20        }
-21
-22        // THE PASS-IT-UP CASE: If one side is null, return the side that ISN'T
-23        // null. (If both are null, this conveniently just returns null anyway).
-24        return leftReport != nullptr ? leftReport : rightReport;
-25    }
-26};
+9
+10        // 2. Send scouts down the left and right branches (Post-Order Traversal)
+11        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+12        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+13
+14        // 3. Post-Processing: Evaluate the reports
+15
+16        // THE WINNING CASE: Both departments found a target. We are the LCA!
+17        if (left && right) 
+18            return root;
+19
+20        // THE PASS-IT-UP CASE: If one side is null, return the side that ISN'T
+21        // null. (If both are null, this conveniently just returns null anyway).
+22        return left ? left : right;
+23    }
+24};
