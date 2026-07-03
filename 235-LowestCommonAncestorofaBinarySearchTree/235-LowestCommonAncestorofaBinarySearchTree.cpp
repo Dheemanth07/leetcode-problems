e812d3d95d0a71c69af3e8c1a9a3d16f97ad7617
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-03 10:07:34 +0530

    File updated at 7/3/2026, 10:07:34 AM

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.cpp
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/10/2026, 4:26:45 PM
+// Last updated: 7/3/2026, 10:07:34 AM
 1class Solution {
 2public:
 3    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -6,19 +6,19 @@
 5
 6        while (curr) {
 7            // 1. Both friends want to go Left
-8            if (p->val < curr->val && q->val < curr->val) {
+8            if (p->val < curr->val && q->val < curr->val) 
 9                curr = curr->left;
-10            }
+10            
 11            // 2. Both friends want to go Right
-12            else if (p->val > curr->val && q->val > curr->val) {
+12            else if (p->val > curr->val && q->val > curr->val) 
 13                curr = curr->right;
-14            }
+14            
 15            // 3. The Fork in the Road!
 16            // One is less, one is greater (or one is exactly equal to curr).
 17            // We found the LCA!
-18            else {
+18            else 
 19                return curr;
-20            }
+20            
 21        }
 22
 23        return nullptr; // Safety net, though the problem guarantees an LCA
