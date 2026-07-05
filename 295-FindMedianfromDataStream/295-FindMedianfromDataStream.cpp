5f68bb55f7530db0d299601ead8c4ad9f8e83091
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-05 20:02:04 +0530

    File updated at 7/5/2026, 8:02:06 PM

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.cpp
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.cpp
@@ -1,4 +1,4 @@
-// Last updated: 1/20/2026, 9:38:19 PM
+// Last updated: 7/5/2026, 8:02:06 PM
 1class MedianFinder {
 2    priority_queue<int, vector<int>> leftMax;
 3    priority_queue<int, vector<int>, greater<int>> rightMin;
@@ -6,32 +6,31 @@
 5public:
 6    MedianFinder() {}
 7
-8    void addNum(int num) { 
-9        if (leftMax.empty() || num < leftMax.top()) 
+8    void addNum(int num) {
+9        if (leftMax.empty() || num < leftMax.top())
 10            leftMax.push(num);
 11        else
 12            rightMin.push(num);
-13        if(leftMax.size()>rightMin.size()+1){
+13        if (leftMax.size() > rightMin.size() + 1) {
 14            rightMin.push(leftMax.top());
 15            leftMax.pop();
-16        }
-17        else if(rightMin.size()>leftMax.size()){
-18            leftMax.push(rightMin.top());
-19            rightMin.pop();
-20        }
-21    }
-22
-23    double findMedian() {
-24        if(leftMax.size()==rightMin.size())
-25            return (leftMax.top()+rightMin.top())/2.0;
-26        else
-27            return leftMax.top();
-28    }
-29};
-30
-31/**
-32 * Your MedianFinder object will be instantiated and called as such:
-33 * MedianFinder* obj = new MedianFinder();
-34 * obj->addNum(num);
-35 * double param_2 = obj->findMedian();
-36 */
+16        } else if (rightMin.size() > leftMax.size()) {
+17            leftMax.push(rightMin.top());
+18            rightMin.pop();
+19        }
+20    }
+21
+22    double findMedian() {
+23        if (leftMax.size() == rightMin.size())
+24            return (leftMax.top() + rightMin.top()) / 2.0;
+25        else
+26            return leftMax.top();
+27    }
+28};
+29
+30/**
+31 * Your MedianFinder object will be instantiated and called as such:
+32 * MedianFinder* obj = new MedianFinder();
+33 * obj->addNum(num);
+34 * double param_2 = obj->findMedian();
+35 */
