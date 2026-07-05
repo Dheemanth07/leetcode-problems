1d13b0cc87b25567f4aca3792d59acfc2969274b
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-05 17:12:41 +0530

    File updated at 7/5/2026, 5:12:41 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.cpp
+++ 1046-LastStoneWeight/1046-LastStoneWeight.cpp
@@ -1,15 +1,17 @@
-// Last updated: 2/21/2026, 8:48:24 PM
+// Last updated: 7/5/2026, 5:12:41 PM
 1class Solution {
 2public:
 3    int lastStoneWeight(vector<int>& stones) {
-4        priority_queue<int> pq(stones.begin(),stones.end());
-5        while (pq.size()>1) {
+4        priority_queue<int> pq(stones.begin(), stones.end());
+5        while (pq.size() > 1) {
 6            int largest = pq.top();
 7            pq.pop();
 8            int seclargest = pq.top();
 9            pq.pop();
-10            if(largest!=seclargest) pq.push(largest-seclargest);
-11        }
-12        return pq.empty()?0:pq.top();
-13    }
-14};
+10            
+11            if (largest != seclargest)
+12                pq.push(largest - seclargest);
+13        }
+14        return pq.empty() ? 0 : pq.top();
+15    }
+16};
