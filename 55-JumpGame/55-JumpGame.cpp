be6f7b580be1ded6e5ea43ea458d9eaebbe05408
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-06 18:20:31 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/6/2026, 6:20:36 PM
+1class Solution {
+2public:
+3    bool canJump(vector<int>& nums) {
+4        int maxReachable = 0;
+5        int n = nums.size();
+6
+7        for (int i = 0; i < n; i++) {
+8            if (i > maxReachable)
+9                return false;
+10            maxReachable = max(maxReachable, i + nums[i]);
+11
+12            if (maxReachable >= n - 1)
+13                return true;
+14        }
+15
+16        return true;
+17    }
+18};
