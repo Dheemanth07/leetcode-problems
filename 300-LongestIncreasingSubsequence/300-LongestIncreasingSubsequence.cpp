960bbb2ddf8c9f16e617ec3b4a6006e8359ba576
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-15 14:43:13 +0530

    File updated at 7/15/2026, 2:43:18 PM

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
@@ -1,23 +1,24 @@
-// Last updated: 7/15/2026, 2:41:29 PM
+// Last updated: 7/15/2026, 2:43:18 PM
 1class Solution {
 2public:
 3    int lengthOfLIS(vector<int>& nums) {
 4        int n = nums.size();
 5
-6        if(!n) return 0;
-7        
-8        vector<int> dp(n + 1, 1);
-9
-10        int maxLIS = 1;
-11
-12        for (int i = 1; i < n; i++) {
-13            for (int j = 0; j < i; j++)
-14                if (nums[i] > nums[j])
-15                    dp[i] = max(dp[i], 1 + dp[j]);
-16                    
-17            maxLIS = max(maxLIS, dp[i]);
-18        }
-19
-20        return maxLIS;
-21    }
-22};
+6        if (!n)
+7            return 0;
+8
+9        vector<int> dp(n, 1);
+10
+11        int maxLIS = 1;
+12
+13        for (int i = 1; i < n; i++) {
+14            for (int j = 0; j < i; j++)
+15                if (nums[i] > nums[j])
+16                    dp[i] = max(dp[i], 1 + dp[j]);
+17
+18            maxLIS = max(maxLIS, dp[i]);
+19        }
+20
+21        return maxLIS;
+22    }
+23};
