bd75f51b7a931452814ec58088d9803060f11e10
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-15 11:05:17 +0530

    File updated at 7/15/2026, 11:05:19 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/15/2026, 10:54:23 AM
+// Last updated: 7/15/2026, 11:05:19 AM
 1class Solution {
 2public:
 3    int longestCommonSubsequence(string text1, string text2) {
@@ -6,17 +6,15 @@
 5
 6        vector<vector<int>> dp(s1 + 1, vector<int>(s2 + 1, 0));
 7
-8        for (int i = 0; i <= s1; i++) {
-9            for (int j = 0; j <= s2; j++) {
-10                if (!i || !j)
-11                    dp[i][j] = 0;
-12                else if (text1[i - 1] == text2[j - 1])
-13                    dp[i][j] = 1 + dp[i - 1][j - 1];
-14                else
-15                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-16            }
-17        }
-18
-19        return dp[s1][s2];
-20    }
-21};
+8        for (int i = 1; i <= s1; i++) {
+9            for (int j = 1; j <= s2; j++) {
+10                if (text1[i - 1] == text2[j - 1])
+11                    dp[i][j] = 1 + dp[i - 1][j - 1];
+12                else
+13                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+14            }
+15        }
+16
+17        return dp[s1][s2];
+18    }
+19};
