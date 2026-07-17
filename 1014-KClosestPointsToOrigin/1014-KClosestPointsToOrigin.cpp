099a56ff5741fae909d3b97ba28098e4ef4ccb26
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-17 14:01:46 +0530

    Create 1014-KClosestPointsToOrigin

---
+++ 1014-KClosestPointsToOrigin/1014-KClosestPointsToOrigin.cpp
@@ -0,0 +1,24 @@
+// Last updated: 7/17/2026, 2:01:49 PM
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
+        int dist, i;
+        for (i = 0; i < points.size(); i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            dist = x * x + y * y;
+            pq.push({dist, i});
+
+            if (pq.size() > k)
+                pq.pop();
+        }
+
+        vector<vector<int>> result;
+        while (!pq.empty()) {
+            result.push_back(points[pq.top().second]);
+            pq.pop();
+        }
+        return result;
+    }
+};
