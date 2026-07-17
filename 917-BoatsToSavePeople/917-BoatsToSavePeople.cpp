ef73e65a6f77a39d5d9162c853fa22caf78fcd98
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-17 14:01:50 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/17/2026, 2:01:53 PM
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int l = 0, r = people.size() - 1;
+        int boats = 0;
+
+        sort(people.begin(), people.end());
+
+        while (l <= r) {
+            if (people[l] + people[r] <= limit)
+                l++;
+            r--;
+            boats++;
+        }
+
+        return boats;
+    }
+};
