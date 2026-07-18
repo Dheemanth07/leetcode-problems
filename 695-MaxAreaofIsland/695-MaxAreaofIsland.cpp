1633e03f156651516ec2beff3a766d30cc1dacc2
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-18 12:14:47 +0530

    File updated at 7/18/2026, 12:14:48 PM

--- 695-MaxAreaofIsland/695-MaxAreaofIsland.cpp
+++ 695-MaxAreaofIsland/695-MaxAreaofIsland.cpp
@@ -1,20 +1,39 @@
-// Last updated: 2/23/2026, 10:38:43 AM
+// Last updated: 7/18/2026, 12:14:48 PM
 1class Solution {
 2public:
-3    int dfs(vector<vector<int>>& grid, int i, int j) {
-4        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() ||
-5            grid[i][j] == 0)
-6            return 0;
-7        grid[i][j] = 0;
-8        return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) +
-9               dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
-10    }
-11
-12    int maxAreaOfIsland(vector<vector<int>>& grid) {
-13        int max_area = 0;
-14        for (int i = 0; i < grid.size(); i++)
-15            for (int j = 0; j < grid[i].size(); j++)
-16                max_area = max(max_area, dfs(grid, i, j));
-17        return max_area;
-18    }
-19};
+3    int maxAreaOfIsland(vector<vector<int>>& grid) {
+4        int rows = grid.size();
+5        int cols = grid[0].size();
+6
+7        int max_area = 0;
+8
+9        for (int i = 0; i < rows; i++) {
+10            for (int j = 0; j < cols; j++) {
+11                if (grid[i][j]) {
+12                    int area = 0;
+13                    max_area = max(max_area, dfs(i, j, grid, area));
+14                }
+15            }
+16        }
+17
+18        return max_area;
+19    }
+20
+21    int dfs(int i, int j, vector<vector<int>>& grid, int& area) {
+22        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
+23            return 0;
+24
+25        if (!grid[i][j])
+26            return 0;
+27
+28        grid[i][j] = 0;
+29        area++;
+30
+31        dfs(i + 1, j, grid, area);
+32        dfs(i, j + 1, grid, area);
+33        dfs(i - 1, j, grid, area);
+34        dfs(i, j - 1, grid, area);
+35
+36        return area;
+37    }
+38};
