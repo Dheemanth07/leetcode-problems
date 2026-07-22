ac3423740bfd07224916ca714f0831be425059c8
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-22 11:13:30 +0530

    File updated at 7/22/2026, 11:13:30 AM

--- 1353-MaximumNumberofEventsThatCanBeAttended/1353-MaximumNumberofEventsThatCanBeAttended.cpp
+++ 1353-MaximumNumberofEventsThatCanBeAttended/1353-MaximumNumberofEventsThatCanBeAttended.cpp
@@ -1,33 +1,52 @@
-// Last updated: 7/6/2026, 5:50:51 PM
+// Last updated: 7/22/2026, 11:13:30 AM
 1class Solution {
 2public:
 3    int maxEvents(vector<vector<int>>& events) {
-4        priority_queue<int, vector<int>, greater<int>> minHeap;
-5        sort(events.begin(), events.end());
-6
-7        int i = 0, n = events.size();
-8        int currentDay = 0;
-9        int attended = 0;
-10
-11        while (i < n || !minHeap.empty()) {
-12            if (minHeap.empty())
-13                currentDay = events[i][0];
-14
-15            while (i < n && events[i][0] == currentDay) {
-16                minHeap.push(events[i][1]);
-17                i++;
-18            }
+4
+5        // Min heap stores the ending days of all events
+6        // that have started but haven't been attended yet.
+7        priority_queue<int, vector<int>, greater<int>> minHeap;
+8
+9        // Sort events by starting day.
+10        sort(events.begin(), events.end());
+11
+12        int i = 0, n = events.size();
+13        int currentDay = 0;
+14        int attended = 0;
+15
+16        // Continue until all events are processed
+17        // and no available events remain.
+18        while (i < n || !minHeap.empty()) {
 19
-20            while (!minHeap.empty() && minHeap.top() < currentDay)
-21                minHeap.pop();
-22            if (!minHeap.empty()) {
-23                attended++;
-24                minHeap.pop();
-25            }
-26
-27            currentDay++;
-28        }
-29
-30        return attended;
-31    }
-32};
+20            // If no event is currently available,
+21            // jump directly to the next event's start day.
+22            if (minHeap.empty())
+23                currentDay = events[i][0];
+24
+25            // Add every event that starts today
+26            // into the heap.
+27            while (i < n && events[i][0] == currentDay) {
+28                minHeap.push(events[i][1]);
+29                i++;
+30            }
+31
+32            // Remove events that have already expired
+33            // (their ending day is before today).
+34            while (!minHeap.empty() && minHeap.top() < currentDay)
+35                minHeap.pop();
+36
+37            // Greedy Choice:
+38            // Attend the event that ends the earliest,
+39            // since delaying it may cause us to lose it forever.
+40            if (!minHeap.empty()) {
+41                attended++;
+42                minHeap.pop();
+43            }
+44
+45            // Move to the next day.
+46            currentDay++;
+47        }
+48
+49        return attended;
+50    }
+51};
