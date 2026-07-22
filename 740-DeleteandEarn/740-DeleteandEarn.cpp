647ac8874a13b5573a0a8f99022d27ddd5d179a4
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-22 21:13:29 +0530

    File updated at 7/22/2026, 9:13:35 PM

--- 740-DeleteandEarn/740-DeleteandEarn.cpp
+++ 740-DeleteandEarn/740-DeleteandEarn.cpp
@@ -1,4 +1,4 @@
-// Last updated: 7/8/2026, 11:08:46 PM
+// Last updated: 7/22/2026, 9:13:35 PM
 1class Solution {
 2public:
 3    int deleteAndEarn(vector<int>& nums) {
@@ -6,28 +6,26 @@
 5            return 0;
 6
 7        // 1. Find the maximum number so we know how long our street needs to be
-8        int max_val = 0;
-9        for (int num : nums) 
-10            max_val = max(max_val, num);
-11
-12        // 2. Build the "Street of Houses"
-13        // The index is the number itself, the value is the total points you'd
-14        // earn taking it
-15        vector<int> street(max_val + 1, 0);
-16        for (int num : nums) 
-17            street[num] += num; // Summing up all occurrences natively!
-18
-19        // 3. Exactly House Robber 1 logic
-20        int prev2 = 0;
-21        int prev1 = 0;
-22
-23        for (int i = 0; i <= max_val; i++) {
-24            // max(Rob It, Skip It)
-25            int current = max(street[i] + prev2, prev1);
-26            prev2 = prev1;
-27            prev1 = current;
-28        }
-29
-30        return prev1;
-31    }
-32};
+8        int max_val = *max_element(nums.begin(),nums.end());
+9
+10        // 2. Build the "Street of Houses"
+11        // The index is the number itself, the value is the total points you'd
+12        // earn taking it
+13        vector<int> street(max_val + 1, 0);
+14        for (int num : nums) 
+15            street[num] += num; // Summing up all occurrences natively!
+16
+17        // 3. Exactly House Robber 1 logic
+18        int prev2 = 0;
+19        int prev1 = 0;
+20
+21        for (int i = 0; i <= max_val; i++) {
+22            // max(Rob It, Skip It)
+23            int current = max(street[i] + prev2, prev1);
+24            prev2 = prev1;
+25            prev1 = current;
+26        }
+27
+28        return prev1;
+29    }
+30};
