b4002658af1d958b8885281041aff22c589e772c
Dheemanth D <165369664+Dheemanth07@users.noreply.github.com>
2026-07-29 17:56:44 +0530

    File updated at 7/29/2026, 5:56:51 PM

--- 1464-MaximumProductofTwoElementsinanArray/1464-MaximumProductofTwoElementsinanArray.cpp
+++ 1464-MaximumProductofTwoElementsinanArray/1464-MaximumProductofTwoElementsinanArray.cpp
@@ -1,9 +1,24 @@
-// Last updated: 7/29/2026, 5:42:32 PM
+// Last updated: 7/29/2026, 5:56:51 PM
 1class Solution {
 2public:
 3    int maxProduct(vector<int>& nums) {
-4        sort(nums.begin(), nums.end());
-5
-6        return (nums[nums.size() - 1] - 1) * (nums[nums.size() - 2] - 1);
-7    }
-8};
+4        int n = nums.size();
+5        int max_ele = 0, second_max_ele = 0;
+6        int idx;
+7
+8        for (int i = 0; i < n; i++) {
+9            if (nums[i] > max_ele) {
+10                max_ele = nums[i];
+11                idx = i;
+12            }
+13        }
+14
+15        for (int i = 0; i < n; i++) {
+16            if (nums[i] == max_ele && i == idx)
+17                continue;
+18            second_max_ele = max(second_max_ele, nums[i]);
+19        }
+20
+21        return (max_ele - 1) * (second_max_ele - 1);
+22    }
+23};
